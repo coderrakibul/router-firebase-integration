ac936fe1be87e610237540dd2ff950ade3497c90
coderrakibul <coder.rakibul@gmail.com>
2022-04-08 23:38:53 +0600

    google sign in added

---
+++ src/component/Register/Register.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h2>Please register now</h2>
+            <form>
+                <input type="text" placeholder='Your Name'/> <br />
+                <input type="email" placeholder='Your Email'/> <br />
+                <input type="password" placeholder='Your Password'/> <br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
